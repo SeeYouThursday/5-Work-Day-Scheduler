494e9a1e1fdf95616d7d6ae0f19a6f5034eb6c6e
Brian <galyenb@vcu.edu>
2023-09-21 16:33:53 -0400

    cleaned up comments and completed README

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # 5-Work-Day-Scheduler
+
+## Description
+
+The user is presented with the current date and hours between 9am-5pm alongside textareas to enter in tasks to be completed at a given time. Through this challenge, I have learned how utilize the JQuery and Dayjs to leverage localStorage for persistant data, while providing the user with a dynamic site.
+
+## Usage
+
+Visit this URL to access this application: <https://seeyouthursday.github.io/5-Work-Day-Scheduler/>
+
+To utilize this daily planner, simply click in any of the textboxes beside a desired time and type in an event. The user will need to click the "Save" button to commit the user input to localStorage, allowing this input to be accessed whenever the user visits the site.
+
+![Developer Portfolio screenshot](./Assets/05-third-party-apis-homework-demo.gif)
+
+## License
+
+Please refer to the LICENSE in the repo.
--- script.js
+++ script.js
@@ -1,13 +1,4 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
   const $saveBtn = $(".saveBtn");
   $saveBtn.on("click", function (event) {
     event.stopPropagation();
@@ -15,18 +6,10 @@ $(function () {
     // returns the id of the parent (time of day div)
     let $task = $(this).prev(this);
     let $taskValue = $task.val();
-    // console.log($taskValue);
     let storedTask = localStorage.setItem($parentID, $taskValue); //works above!
     $(function getStored($parentID) {
       let getStoredTask = localStorage.getItem($parentID);
-      // $("textarea").text(getStoredTask);
     });
-
-    // const
-    // the browser to check to see if there is a key to match the textarea's parent's id (hour-#)
-    // if key = parent id, then textarea.text(value)
-    // if ()
-    // how can I future proof this in case the position of textarea changes later?
   });
 
   // renders tasks to their respective textareas
@@ -47,7 +30,6 @@ $(function () {
     $timeBlock.each(function () {
       const $this = $(this);
       let $thisAttr = $this.attr("data-hour");
-      // let $thisAttrValue = $this[(id*=11)];
       console.log($thisAttr);
       if ($thisAttr === currentHour) {
         $this.addClass("present");
